3c857cc06a7302f5b28b1342cf2e56f8187b4d3e
entran02 <evanntran2002@gmail.com>
2024-12-04 19:32:57 -0500

    gitignore

---
+++ rust/target/debug/deps/rust-406030a7c62b0458.d
@@ -0,0 +1,5 @@
+C:\Users\14138\Desktop\solitaire-ocr\rust\target\debug\deps\librust-406030a7c62b0458.rmeta: src/main.rs
+
+C:\Users\14138\Desktop\solitaire-ocr\rust\target\debug\deps\rust-406030a7c62b0458.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/deps/rust-f80b2eb40aecbfec.d
@@ -0,0 +1,5 @@
+C:\Users\14138\Desktop\solitaire-ocr\rust\target\debug\deps\librust-f80b2eb40aecbfec.rmeta: src/main.rs
+
+C:\Users\14138\Desktop\solitaire-ocr\rust\target\debug\deps\rust-f80b2eb40aecbfec.d: src/main.rs
+
+src/main.rs:
